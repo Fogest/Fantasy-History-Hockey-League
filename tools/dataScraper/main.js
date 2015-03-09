9fd2181927dbeff1db9ea148959620208ffd080a
Justin Visser <justin@jhvisser.com>
2015-03-08 22:26:29 -0400

    Now scrapes team data

--- tools/dataScraper/main.js
+++ tools/dataScraper/main.js
@@ -7,7 +7,6 @@ var fs = require('fs');
 function doResultsRequest(year, dir, url, csv) {
 	request(url, function(err, resp, data){
 		if (!err) {
-			//console.log(data);
 			var $ = cheerio.load(data);
 			
 			csv += "date,vistor,vistorAbrv,vistorGoals,home,homeAbrv,homeGoals,notes";
@@ -50,7 +49,6 @@ function doResultsRequest(year, dir, url, csv) {
 function doPlayersRequest(year, dir, url, csv) {
 	request(url, function(err, resp, data){
 		if (!err) {
-			//console.log(data);
 			var $ = cheerio.load(data);
 			
 			csv += "rank,player,age,teamName,position,gamesPlayed,goals,assists,points,plusMinus,penaltyMinutes,evenStrengthGoals,powerPlayGoals,shortHandedGoals,gameWinningPoints,evenStrengthAssists,powerPlayAssits,shortHandedAssists,shots,shootingPercentage,timeOnIce,averageTimeOnIce\n";
@@ -82,6 +80,48 @@ function doPlayersRequest(year, dir, url, csv) {
 	});
 }
 
+function doTeamRequest(year, dir, url, csv) {
+	request(url, function(err, resp, data){
+		if (!err) {
+			var $ = cheerio.load(data);
+			
+			csv += "rank,teamName,teamAbrv,gamesPlayed,wins,losses,ties,points,pointsPercentage,goalsFor,goalsAgainst,simpleRatingSystem,strengthOfSchedule,totalGoalsPerGame,powerPlayGoals,powerPlayChances,powerPlayPercentage,powerPlayGoalsAgainst,powerPlayChancesAgainst,penaltyKillingPercentage,shortHandedGoals,shortHandedGoalsAgainst,shots,shootingPercentage,shotsAgainst,savePercentage,pdo\n";
+			$("#div_teams table tbody tr").each(function() {
+				//Only take if not one of the headings.
+				if(!($(this).hasClass("no_ranker"))) {
+					$('td',this).each(function(index,element) {
+						if(index >= 25)
+							var field = '"' + $(this).text().replace('*','') + '"';
+						else
+							var field = '"' + $(this).text().replace('*','') + '"' + ',';
+						csv += field;
+						
+						if(index == 1) {
+							var re = /\/([A-Z]*)\//g; 
+							var match = re.exec($("a",this).attr("href"));
+							csv += '"' + match[1] + '"' + ",";
+						}
+					});
+					csv += "\n";
+				}
+			});
+			
+			if (!fs.existsSync(dir)){
+				fs.mkdirSync(dir);
+			}
+			
+			fs.writeFile(dir + "teams.csv", csv, function(err) {
+				if(err) {
+					console.log(err);
+				} else {
+					console.log("The file was saved!");
+				}
+			});
+		}
+	});
+}
+
+
 function getResults() {
 	var year = 1917;
 	var dir = "";
@@ -89,13 +129,18 @@ function getResults() {
 	var csv = "";
 	while (year < 2015) {
 		dir = "../../data/" + year + "/";
-		url = "http://www.hockey-reference.com/leagues/NHL_"+ (year+1) +"_games.html";
 		
+		url = "http://www.hockey-reference.com/leagues/NHL_"+ (year+1) +"_games.html";
 		csv = "";
 		doResultsRequest(year, dir, url, csv);
+		
 		url = "http://www.hockey-reference.com/leagues/NHL_"+ (year+1) +"_skaters.html";
 		csv = "";
 		doPlayersRequest(year, dir, url, csv);
+		
+		url = "http://www.hockey-reference.com/leagues/NHL_"+ (year+1) +".html";
+		csv = "";
+		doTeamRequest(year, dir, url, csv);
 		
 		year++;
 	}
@@ -108,9 +153,9 @@ function testing() {
 	var csv = "";
 	
 	dir = "../../data/" + year + "/";
-	url = "http://www.hockey-reference.com/leagues/NHL_"+ (year+1) +"_skaters.html";
+	url = "http://www.hockey-reference.com/leagues/NHL_"+ (year+1) +".html";
 	csv = "";
-	doPlayersRequest(year, dir, url, csv);
+	doTeamRequest(year, dir, url, csv);
 	
 	year++;
 }
