fb367247e905f2fdc38be3649f677f612955773b
Dark <nfriday@git.socs.uoguelph.ca>
2015-03-17 14:18:11 -0400

    started making metrics file

---
+++ src/metrics.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+#test Variables (temp since no input)
+
+@teamNames = ("OTS","MTL","TRA");
+@
+@teamWins = (12,10,5);
+@teamLosses(6,8,13);
+@teamPoints(24,20,10);
+my $gamesPlayed= 18
+
+sub 
+{
+    
+}
