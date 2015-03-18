15c70c36c4e307ddd59da1b2ecc5d7920ca91a3b
Justin V <fogestjv@gmail.com>
2015-03-18 12:47:40 -0400

    Fixing goalie data again

--- tools/dataScraper/main.js
+++ tools/dataScraper/main.js
@@ -90,7 +90,7 @@ function doGoaliesRequest(year, dir, url, csv) {
 				//Only take if not one of the headings.
 				if(!($(this).hasClass("no_ranker"))) {
 					$('td',this).each(function(index,element) {
-						if(index >= 23)
+						if(index >= 24)
 							var field = '"' + $(this).text().replace('*','') + '"';
 						else
 							var field = '"' + $(this).text().replace('*','') + '"' +',';
