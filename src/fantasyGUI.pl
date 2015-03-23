24574eca6bc2d6e3d0ee4598e2e65d11306b8790
Joshua Lange <jlange@mail.uoguelph.ca>
2015-03-22 20:51:42 -0400

    Fixed errors and such, screen clears and error messages are displayed

--- src/fantasyGUI.pl
+++ src/fantasyGUI.pl
@@ -1,17 +1,23 @@
 #!/usr/bin/perl
+use strict;
+#use warnings;
 
-$usrInput1 = 0;
-$usrInput2 = 0;
-$usrInput3 = 0;
-$usrInput4 = 0;
-$usrYearInput = 1900;
-$usrTeamInput = "default";
+use Text::CSV;
+
+my $usrInput1 = 0;
+my $usrInput2 = 0;
+my $usrInput3 = 0;
+my $usrInput4 = 0;
+my $usrYearInput = 1900;
+my $usrTeamInput = "default";
+
+
 while ($usrInput1 != 3)
 {
-    $usrInput2 = 0;
-    print "|----------------------------|\n";
-    print "| Welcome to Fantasy Hockey  |\n";
-    print "|----------------------------|\n";
+    $usrInput2 = 0; 
+    print "|-----------------------------|\n";
+    print "|- Welcome to Fantasy Hockey- |\n";
+    print "|-----------------------------|\n";
 
     print "1. Dashboard\n";
     print "2. Play Game\n";
@@ -19,12 +25,14 @@ while ($usrInput1 != 3)
 
     print "Enter the number of your choice:";
     $usrInput1 = <>;
+    print "\033[2J";    #clear the screen
+    print "\033[0;0H"; #jump to 0,0
     if ($usrInput1 == 1)
     {
         while ($usrInput2 !=3)
         {
             $usrYearInput = 1900;
-            print "\n\n|----------------------------|\n";
+            print "|----------------------------|\n";
             print "|---------Dashboard----------|\n";
             print "|----------------------------|\n\n";
 
@@ -33,20 +41,30 @@ while ($usrInput1 != 3)
             print "2.Search by team\n";
             print "3.Return to Main Menu\n\n";
             print "Enter the number of your choice:";
-            $usrInput2= <>;
-            
+            $usrInput2= <>; 
+            print "\033[2J";    #clear the screen
+            print "\033[0;0H"; #jump to 0,0            
+
             if ($usrInput2 == 1)
             {
-                while ($usrYearInput != 0)
+            while ($usrYearInput != 0)
                 {
-                    print "Enter the year that you wish to search for, or 0 to exit:";
+                    print "Enter a year between 1917 and 2014 that you wish to search for, or 0 to exit:";
                     $usrYearInput= <>; 
+                    print "\033[2J";    #clear the screen
+print "\033[0;0H"; #jump to 0,0
+
+                    while ((($usrYearInput < 1917) || ($usrYearInput > 2014)) && ($usrYearInput != 0)) {
+                        print "Bad Input, please choose a number between 1917 and 2014: ";
+                        $usrYearInput= <>; 
+                        print "\033[2J";    #clear the screen
+                        print "\033[0;0H"; #jump to 0,0 
+                    }
                 }
             }
             if ($usrInput2 == 2)
             {
-                $usrTeamInput = "default";
-                while ($usrTeamInput ne "exit")
+                $usrTeamInput = "default"; while ($usrTeamInput ne "exit")
                 {
                     print "Here is a list of teams to choose from:";
 
@@ -55,10 +73,12 @@ while ($usrInput1 != 3)
 
                     print "\n\nEnter the abreviation you want to use, or type exit to return to the main menu:";
                     $usrTeamInput= <>;
+                    print "\033[2J";    #clear the screen
+                    print "\033[0;0H"; #jump to 0,0
                     chomp ($usrTeamInput);
                     if ($usrTeamInput eq "exit")
                     {
-                        $usrInput2 = 0; 
+                        $usrInput2 = 0;
                     } 
                 }
             }
