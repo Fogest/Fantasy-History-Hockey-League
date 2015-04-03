30e670dbb169f7de9df0543bff93cde6bda5df1e
Justin V <fogestjv@gmail.com>
2015-04-03 11:01:31 -0400

    AHHHHHHHHHHHHHHH

--- src/fantasyGUI.pl
+++ src/fantasyGUI.pl
@@ -191,11 +191,12 @@ while ($usrInput1 != 4)
                 }                
                 if (($usrYearInput > 1916) && ($usrYearInput < 2015))
                 {
-                    my $yearFile = "../data/$usrYearInput/teams.csv";                        
+                    my $yearFile = "../data/$usrYearInput/teams.csv";                       
+                    print 'hi'; 
                     open (my $yearToTeamFH, "<",$yearFile) 
-                          
                         or die "Unable to open the required year's file: $yearFile";
                      
+                     print 'there';
                     my $yearToTeamRecord = <$yearToTeamFH>;
                     my $teamCount=0;
                     $tempCount=0; 
